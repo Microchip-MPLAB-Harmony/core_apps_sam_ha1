a5b36c77e15d37d0d7915897ba09854503d1541c
Vishal Gandhi <vishal.gandhi@microchip.com>
2022-05-29 15:45:35 +0530

    [DRV_SPI] Regenerated demo for driver_spi_async_spi_self_loopback_multi_client_sam_ha1_xpro
    MH3-65540
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/driver/spi/async/spi_self_loopback_multi_client/firmware/src/config/sam_ha1_xpro/peripheral/port/plib_port.c
+++ apps/driver/spi/async/spi_self_loopback_multi_client/firmware/src/config/sam_ha1_xpro/peripheral/port/plib_port.c
@@ -52,7 +52,6 @@
 // Section: PORT Implementation
 // *****************************************************************************
 // *****************************************************************************
-
 // *****************************************************************************
 /* Function:
 
@@ -81,16 +80,19 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[0].PORT_PINCFG[11] = 0x1;
    PORT_REGS->GROUP[0].PORT_PINCFG[16] = 0x0;
 
+   PORT_REGS->GROUP[0].PORT_PMUX[0] = 0x0;
    PORT_REGS->GROUP[0].PORT_PMUX[2] = 0x33;
    PORT_REGS->GROUP[0].PORT_PMUX[5] = 0x20;
+   PORT_REGS->GROUP[0].PORT_PMUX[8] = 0x0;
 
    /************************** GROUP 1 Initialization *************************/
    PORT_REGS->GROUP[1].PORT_OUT = 0x8;
    PORT_REGS->GROUP[1].PORT_PINCFG[3] = 0x6;
 
+   PORT_REGS->GROUP[1].PORT_PMUX[1] = 0x0;
+
 
 }
-
 // *****************************************************************************
 /* Function:
     uint32_t PORT_GroupRead(PORT_GROUP group)
