02c5f1728da60650fdbdb56bb31b8ddd6e38f287
Syam Nair <Syam.Nair@microchip.com>
2025-09-23 09:13:53 +0000

    [APP] Regenerated demo for rtos__freertos__basic_freertos__firmware
    MH3C-347

--- apps/rtos/freertos/basic_freertos/firmware/src/config/sam_ha1_xpro/definitions.h
+++ apps/rtos/freertos/basic_freertos/firmware/src/config/sam_ha1_xpro/definitions.h
@@ -14,7 +14,7 @@
 
 //DOM-IGNORE-BEGIN
 /*******************************************************************************
-* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+* Copyright (C) 2025 Microchip Technology Inc. and its subsidiaries.
 *
 * Subject to your compliance with these terms, you may use Microchip software
 * and any derivatives exclusively with Microchip products. It is your
@@ -54,8 +54,8 @@
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
-#include "peripheral/eic/plib_eic.h"
 #include "bsp/bsp.h"
+#include "peripheral/eic/plib_eic.h"
 #include "FreeRTOS.h"
 #include "task.h"
 #include "osal/osal.h"
